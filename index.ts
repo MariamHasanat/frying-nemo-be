1ea0e8c963e312b56d55ac82e7fe2cb2d4874f5c
Lana <lanaqawasmy7@gmail.com>
2023-03-12 13:07:29 +0200

    update item import

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import { Item } from './models/index.js';
+import { Item } from './models';
 
 dotenv.config();
 
