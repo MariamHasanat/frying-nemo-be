f71489e01ca83adbc787caf828d805f95c695dfe
Manolya Tamimi <tamimimanolya@gmail.com>
2023-03-12 11:51:39 +0200

    connecting to database

--- dist/index.js
+++ dist/index.js
@@ -8,4 +8,11 @@ app.get('/', (req, res) => {
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    connectToDB();
 });
+const connectToDB = () => {
+    console.log("connecting");
+    mongoose.connect("mongodb://127.0.0.1:27017")
+        .then(() => console.log('successfully connected to database!'))
+        .catch((err) => console.log(`something went wrong ;-(\n${err}`));
+};
