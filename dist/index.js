ae0c3a445dfea192fe57439de214cea27e208142
Mariam <201011@ppu.edu.ps>
2023-03-12 11:55:40 +0200

    connecting to mongodb by mongoose

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
+import mongoose from 'mongoose';
 const app = express();
 const port = process.env.PORT || 3001;
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
+const dbConnect = () => {
+    mongoose.connect("mongodb://127.0.0.1:27017/frying-nemo-db")
+        .then(() => {
+        console.log(`[server] : connected to mongodb`);
+    })
+        .catch((error) => {
+        console.log(`⚡️[server] : failed to connect to mongodb ${error}`);
+    });
+};
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    dbConnect();
 });
