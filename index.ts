24cf747341b89605fb71383d5826462fc9312cb4
3bdalazeez <191021@ppu.edu.ps>
2023-03-12 11:03:53 +0200

    new comment

--- index.ts
+++ index.ts
@@ -1,15 +1,64 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import express, { Express, json, Request, Response } from 'express';
+import mongoose, { Model, Schema, Document } from 'mongoose';
 
 const app: Express = express();
-const port = process.env.PORT || 3001;
+const port = 3006;
+app.use(json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server + Hello World');
+mongoose.connect('mongodb://127.0.0.1:27017/todo', {});
+const database = mongoose.connection;
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
+database.on('error', (err) => {
+  console.error(err)
+});
+
+interface ITask {
+  title: String,
+  status: String,
+  date: String,
+}
+
+const tasksSchema = new mongoose.Schema<ITask>({
+  title: String,
+  status: String,
+  date: String,
+}, { versionKey: false });
+
+const Task = mongoose.model('tasks', tasksSchema);
+
+
+app.get('/', async (req: Request, res: Response) => {
+  const result = await Task.find();
+  res.send(result);
+});
+
+app.get('/:id', async (req: Request, res: Response) => {
+  const result = await Task.find();
+  res.send(result);
+});
+
+
+app.post('/', async (req, res) => {
+
+  const item = new Task({
+    title: req.body.title,
+    status: req.body.status,
+    date: req.body.date,
+  });
+
+  try {
+    item.save();
+    res.status(200).send('Item added successfully');
+  } catch (error: any) {
+    res.status(500).send(error.message).end()
+    console.error(error.message);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: server is running at http://localhost:${port}`);
 });
