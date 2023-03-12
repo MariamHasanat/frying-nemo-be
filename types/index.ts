7aac9cb55eb1f8f8b46dbe036eefac04a735772d
abdullah-nuaimi <181004@ppu.edu.ps>
2023-03-12 13:51:09 +0200

    add types to item

---
+++ types/index.ts
@@ -0,0 +1,12 @@
+interface IItem {
+    name: string;
+    imageUrl: string;
+    description: string;
+    price: number;
+    category: string;
+    ingredients: string[];
+};
+
+export interface IItemRequest extends Express.Request{
+    body: IItem
+};
