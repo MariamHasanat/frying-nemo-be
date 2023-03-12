554627cd8a4a96e953eb31387af2df62f258c264
abdullah-nuaimi <181004@ppu.edu.ps>
2023-03-12 11:49:54 +0200

    initialize connection to loacl mongodb

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
-app.get('/', (req, res) => {
-    res.send('Express ' + 'TypeScript Server ' + 'Hello World');
+app.get("/", (req, res) => {
+    res.send("Express " + "TypeScript Server " + "Hello World");
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    dbConnect();
 });
+const dbConnect = () => {
+    console.log('⚡️[server]: connecting to DB...');
+    mongoose
+        .connect("mongodb://127.0.0.1:27017/frying-nemo")
+        .then(() => {
+        console.log("⚡️[server]: connected to DB 📦");
+    })
+        .catch((err) => {
+        console.log(`⚡️[server]: Failed to connected to DB 📦 ${err}`);
+    });
+};
--- index.ts
+++ index.ts
@@ -1,15 +1,29 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express ' + 'TypeScript Server ' + 'Hello World');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express " + "TypeScript Server " + "Hello World");
 });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  dbConnect();
 });
+
+const dbConnect = () => {
+  console.log('⚡️[server]: connecting to DB...')
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/frying-nemo")
+    .then(() => {
+      console.log("⚡️[server]: connected to DB 📦");
+    })
+    .catch((err) => {
+      console.log(`⚡️[server]: Failed to connected to DB 📦 ${err}`);
+    });
+};
