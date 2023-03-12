fee3f92541c43368cddd4194a57d522401b25ced
dalabilal <dalabilal@gmail.com>
2023-03-12 13:04:19 +0200

    update ignore

--- index.ts
+++ index.ts
@@ -1,48 +1,24 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import { Item } from './models/index.js';
+import express , {Express , Response , Request} from 'express'
+import mongoose from 'mongoose'
 
-dotenv.config();
+const app : Express = express();
+const port = process.env.Port || 3001;
 
-const app: Express = express();
-const port = process.env.PORT || 3001;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Your Server is working fine!');
-});
 
-app.get('/createItemTmp', (req: Request, res: Response) => {
-
-  const newItem = new Item({
-    name: 'Qidra',
-    category: 'Main Dish',
-    ingredients: ['rice', 'water', 'salt', 'chicken'],
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam architecto illo facere, officiis ab, vitae itaque voluptatem repudiandae perspiciatis',
-    price: 20.5
-  });
-
-  newItem.save()
-    .then(() => {
-      res.send("Item is added :/");
-    })
-    .catch((err: mongoose.Error) => {
-      res.status(500).send("Failed to add :( " + err.message);
-    })
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://127.0.0.1:${port}`);
-  dbConnect();
-});
+app.listen(port , () => {
+    console.log(`[server] : Server is running at mongodb://localhost:27017`);
+    dbConnect();
+})
 
 const dbConnect = () => {
-  console.log("connecting to db...");
-  mongoose.connect("mongodb://127.0.0.1:27017/frying-nemo")
+    console.log("Connecting to db...");
+    mongoose.connect("mongodb://127.0.0.1:27017/frying-nemo")
     .then(() => {
-      console.log(`🤗 [server]: Connected to MongoDB`);
+        console.log(` [sever]: Connected to MongoDB`);
     })
     .catch((err) => {
-      console.log(`🤨 [server]: Failed to connect to mongodb ${err}`);
-    });
+        console.log(`[server] : Faild to connect to mongodb ${err}`);
+        
+    })
 }
