07c95b879939e38c69dfb7f535b12945b74929fd
NadeenJubeh <191080@ppu.edu.ps>
2023-03-16 13:55:34 +0200

    update item controllers

--- controllers/item.controllers.ts
+++ controllers/item.controllers.ts
@@ -1,8 +1,34 @@
+import mongoose from "mongoose";
 import Item from "../models/item.model"
 import { IItemQuery } from "../types/index";
 
 const getItems = async (qs: IItemQuery) => {
-    const items = await Item.find();
+    const query: mongoose.FilterQuery<typeof Item > ={}; 
+    if (qs.maxPrice !== undefined) {
+        query.price = { $lte: qs.maxPrice }
+    }
+    if (qs.category) {
+        query.category = { $eq: qs.category }
+    }
+    if (qs.page !== undefined) {
+        query.page = { $eq: qs.page }
+    }
+    if (qs.searchTerms) {
+        query.$or = [
+            {
+                name : new RegExp (qs.searchTerms,'i') 
+            },
+            {
+                description : new RegExp (qs.searchTerms,'i')
+            },{
+                category: new RegExp (qs.searchTerms,'i')
+            },
+
+        ]
+    }
+    console.log(query);
+    
+    const items = await Item.find(query);
     return items;
 }
 const createItem = (data: any) => {
