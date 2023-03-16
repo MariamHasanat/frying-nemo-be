5401f64e2ad6fac514ef895da46c4fb801d06ad4
Momen-G-Ar <cp.momen@gmail.com>
2023-03-16 13:34:32 +0200

    add logger

--- index.ts
+++ index.ts
@@ -3,13 +3,14 @@ import dotenv from 'dotenv';
 import { Item } from './models/index';
 import mongoose, { MongooseError } from 'mongoose';
 import { itemsRouter } from './routers/index';
+import { logger } from './middleware/index';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 app.use(express.json());
-
+app.use(logger);
 
 app.use('/items', itemsRouter);
 
