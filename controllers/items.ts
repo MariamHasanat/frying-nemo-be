08511b1976a2fdf6f71563df31a5e29693e5eda4
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-03-16 14:00:49 +0200

    apply or , line 12 need to be fixed

--- controllers/items.ts
+++ controllers/items.ts
@@ -1,18 +1,22 @@
+import mongoose from "mongoose";
 import Item from "../models/items";
 import { MenuItem } from "../types/index";
 
 const getItems = async (params: MenuItem.IQuery) => {
-    let query = {}
-
+    let query: mongoose.FilterQuery<typeof Item> = {}
     if (params.maxPrice !== undefined)
         query = { ...query, price: { $lte: params.maxPrice } }
     if (params.category !== undefined)
-        query = { ...query, category : {$eq: params.category} }
-    if (params.maxPrice !== undefined)
-        query = { ...query, price: { $lte: params.maxPrice } }
-    if (params.searchTerms !== undefined)
-        query = { ...query, name: (new RegExp(params.searchTerms , 'i')) }
-    
+        query = { ...query, category: { $eq: params.category } }
+    if (params.searchTerms !== undefined) {
+        const qRegex = new RegExp(params.searchTerms, 'i');
+        query = { ...query, name: qRegex }
+        query.$or = [
+            { name: qRegex },
+            { category: qRegex },
+            { description: qRegex }
+        ]
+    }
     return await Item.find(query); //it returns the items as array of js objects
 }
 
