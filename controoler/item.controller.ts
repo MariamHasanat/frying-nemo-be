af86f0d7836b8aa186cb1a11ef7269de0aa4d86c
sajedaswad <you@example.com>
2023-03-16 13:52:48 +0200

    use  or query  to return  all thing have the same what i serch term on it

--- controoler/item.controller.ts
+++ controoler/item.controller.ts
@@ -1,22 +1,32 @@
+import mongoose from "mongoose";
 import Item from "../models/items.moddel.js";
 import { ItemQuery } from "../types/index.js";
 
 
 const getItem = async (params: ItemQuery) => {
-//    const quary :any = {};
+    const query: mongoose.FilterQuery<typeof Item> = {};
 
-//    if ( params.maxPrice !== undefined){
-//     quary.price = {$lte : params.maxPrice}
-//    }
-
-
-    const items = await Item.find({
-     price:{
-    $lte :20
-    //params.maxPrice less than  or equal 
-     }
+    if (params.maxPrice !== undefined) {
+        query.price = { $lte: params.maxPrice }  //params.maxPrice less than  or equal 
     }
-    );//return all items as a json if just () else no return specific 
+
+    if (params.searchTerms) {
+        const qReg = new RegExp(params.searchTerms, 'i')//i for case insinsetive , regex : make easer to put condition  
+
+
+        query.$or = [
+            { name: qReg },
+            { description: qReg },
+            { category: qReg }
+        ]
+    }
+    if (params.category) {
+        query.category = { $eq: params.category }
+    }
+    console.log(JSON.stringify(query));
+
+
+    const items = await Item.find(query);//return all items as a json if just () else no return specific 
     return (items);
 }
 
