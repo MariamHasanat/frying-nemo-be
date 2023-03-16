c42f5e057924520e76a90ad29a87dbb3a6dacaa9
Manolya Tamimi <tamimimanolya@gmail.com>
2023-03-16 13:56:20 +0200

    added and fixed filters on item

--- controllers/item.controller.ts
+++ controllers/item.controller.ts
@@ -1,12 +1,29 @@
+import mongoose from "mongoose";
 import { Item } from "../models/index";
 import { IItem, IItemQuery } from "../types/index";
 
 const getItems = async (query: IItemQuery) => {
-    const items = await Item.find({
-        price: {
-            $lte: query.maxPrice
-        }
-    });
+    const q: mongoose.FilterQuery<IItem> = {} // next session: generic types of typescript
+    
+    if (query.maxPrice !== undefined) {
+        q.price = { $lte: query.maxPrice }
+    }
+
+    if (query.category) {
+        q.category = { $eq: query.category }
+    }
+
+    if (query.searchTerms) {
+        const RegEx = new RegExp(query.searchTerms, 'i');
+        q.$or = [
+            { name: RegEx },
+            { description: RegEx },
+            { category: RegEx },
+        ]
+
+    }
+
+    const items = await Item.find(q);
     return items;
 }
 
@@ -19,7 +36,7 @@ const createItem = (body: IItem) => {
         description: body.description,
         imageURL: body.imageURL
     })
-    
+
     return newItem.save() // returning a promise
 }
 
