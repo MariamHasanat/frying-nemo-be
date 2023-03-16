a8faf54a8269f99857a09266cf90bba6b3af182a
Ayat AbuAllan <ayat.online99@hotmail.com>
2023-03-16 13:51:03 +0200

    add multiple query

--- controllers/items.controllers.ts
+++ controllers/items.controllers.ts
@@ -1,12 +1,37 @@
+import mongoose from "mongoose";
 import Item from "../models/item.moudel";
-import { IIQuery } from "../types/index";
+import { IIQuery, IItem } from "../types/index";
 
 const getItems = async (params: IIQuery) => {
+    const query: mongoose.FilterQuery<IItem> = {}
 
-    const items = await Item.find();  //select all from table
+    if (params.maxPrice !== undefined) {
+        query.price = { $lte: params.maxPrice }
+    }
+    if (params.category) {
+        query.category = { $eq: params.category }
+    }
+    if (params.searchTerms) {
+
+        const qReg = new RegExp(params.searchTerms, 'i')
+        query.$or = [
+            {
+                name: qReg
+            }, {
+                description: qReg
+            },
+            {
+                category: qReg
+            }
+        ]
+
+    }
+
+
+    const items = await Item.find(query);
+
     return items;
 }
-
 
 const createItems = (data: any) => {
 
