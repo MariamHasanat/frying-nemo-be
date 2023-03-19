f7d5bebc3344c1b00b9f577cad958f32b2919f93
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-03-19 13:20:00 +0200

    use generic types

--- types/index.ts
+++ types/index.ts
@@ -2,23 +2,26 @@ import express from 'express'
 
 export namespace MenuItem {
     export interface IItem {
-        name : string ,
-        imageUrl : string ,
-        description : string , 
-        price : number , 
-        category : string ,
-        ingredients : string[]
+        name: string,
+        imageUrl: string,
+        description: string,
+        price: number,
+        category: string,
+        ingredients: string[]
     }
-    
+
     export interface IQuery {
-        category? : string ;
-        searchTerms? : string ;
-        page? : number ;
-        maxPrice? : number; 
+        category?: string;
+        searchTerms?: string;
+        page?: number;
+        maxPrice?: number;
     }
-    
-    export interface IRequest extends express.Request {
-        body : IItem
-    }
+
+    // export interface IRequest extends express.Request {
+    //     body : IItem
+    // }
+
+    // to set the type of request body to (IItem) , and the request query string to (IQuery) 
+    export interface IRequest extends express.Request<{}, {}, IItem, IQuery , {}> { }
 
 }
