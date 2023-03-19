42a55234238f5f6848b3d6011b04c5a7fa623c38
abdullah-nuaimi <181004@ppu.edu.ps>
2023-03-19 12:26:55 +0200

    add namespace to menuItems

--- types/index.ts
+++ types/index.ts
@@ -1,19 +1,23 @@
 import express from "express";
 
-export interface IItem {
-    name: string;
-    imageUrl: string;
-    description: string;
-    price: number;
-    category: string;
-    ingredients: string[];
-};
-export interface IItemQuery{
-    category?: string;
-    searchTerms?: string;
-    page?: number;
-    maxPrice?: number;
-}
-export interface IItemRequest extends express.Request{
-    body: IItem
+
+export namespace MenuItem {
+
+    export interface IItem {
+        name: string;
+        imageUrl: string;
+        description: string;
+        price: number;
+        category: string;
+        ingredients: string[];
+    };
+    export interface IItemQuery{
+        category?: string;
+        searchTerms?: string;
+        page?: number;
+        maxPrice?: number;
+    }
+    export interface IItemRequest extends express.Request{
+        body: IItem
+    };
 };
