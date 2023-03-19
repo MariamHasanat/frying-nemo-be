25137c78c7a1ba44b8b83a845e8ba6fa521b0dac
Lana <lanaqawasmy7@gmail.com>
2023-03-19 12:33:43 +0200

    Add namespaces to types

--- types/index.ts
+++ types/index.ts
@@ -1,26 +1,36 @@
 import express from "express";
 
+export namespace MenuItem {
+    export interface Item {
+        name: string;
+        imageUrl: string;
+        description: string;
+        price: number;
+        category: string;
+        ingredients: string[];
+    }
 
-export interface IItem {
-    name: string;
-    imageUrl: string;
-    description: string;
-    price: number;
-    category: string;
-    ingredients: string[];
+
+    export interface ItemQuery {
+        category?: string;
+        searchTerms?: string;
+        page?: number;
+        maxPrice?: number;
+    }
+
+    export interface ItemRequest extends express.Request {
+        body: Item
+    }
 }
-
-
-export interface IItemQuery {
-    category?: string;
-    searchTerms?: string;
-    page?: number;
-    maxPrice?: number;
-}
-
-export interface IItemRequest extends express.Request {
-    body: IItem
-}
-
 // export interface IItemRequest extends express.Request<{}, {}, IItem, IItemQuery> {
 // }
+export namespace User {
+    export interface IUser {
+        email: string;
+        password: string;
+        role: string;
+        fullName: string;
+        imageUrl: string;
+        authToken: string;
+    }
+}
