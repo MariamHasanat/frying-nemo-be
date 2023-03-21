639ae2587a458e9e80f9d642575339db402ef5aa
abdullah-nuaimi <181004@ppu.edu.ps>
2023-03-21 13:05:24 +0200

    install CORS

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { Item } from "./models/index";
 import { itemRouter } from "./routes/index";
+import cors from 'cors'
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use("/items", itemRouter);
 app.get("/", (req: Request, res: Response) => {
