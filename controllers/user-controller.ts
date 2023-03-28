74728f52670b193611359238a16604096f3fddfb
Mariam <201011@ppu.edu.ps>
2023-03-28 14:39:42 +0300

    changes on user schema and user controller

---
+++ controllers/user-controller.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { User } from "../models/index";
+import IUser from "../interfaces/user-interface";
+const createUser = (user : IUser) => {
+   
+
+   
+}
+
+export default {  createUser };
--- models/user-schema.ts
+++ models/user-schema.ts
@@ -2,18 +2,31 @@ import mongoose from "mongoose";
 import IUser from "./../interfaces/user-interface";
 const UserSchema = new mongoose.Schema<IUser>(
     {
-        id: Number,
-        email: { type: String },
-        password: { type: String },
-        role: { type: String },
-        fullName: { type: String },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            default: 'user'
+        },
+        fullName: {
+            type: String,
+            required: true
+        },
         imageUrl: { type: String },
-        authToken: {type: String}
-    }, {versionKey : false});
+        authToken: { type: String }
+    }, { versionKey: false });
 
 // create the model using this schema
-const Item = mongoose.model("Item", UserSchema);
+const User = mongoose.model("User", UserSchema);
 //will create the table in db ,  and return some functions to deal with table 
 
 
-export default Item;
+export default UserSchema ;
